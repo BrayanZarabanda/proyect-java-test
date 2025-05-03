3720baef061264337b76c87be25066d9b0cf172f
BrayanZarabanda <brayan_zarabanda@soy.sena.edu.co>
2025-05-02 20:37:21 -0500

    modificar su programa de Java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!Welcome to Java programming.");
+        scanner.close();
     }
 }
